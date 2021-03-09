e08ae192fccad87ca4bc7fef3982a9e080946fa8
devangpatel94 <44684009+devangpatel94@users.noreply.github.com>
2021-03-09 11:04:27 -0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 // Map tile layers
-// grayscale background
+// Grayscale background
 var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
 	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 	maxZoom: 18,
